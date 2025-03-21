bdc631a4dfe4374cf97a598c54fb6c6753813a15
kwoong2001 <kwoong2001@gmail.com>
2025-03-21 11:08:54 +0900

    250321_v3

--- Basic/Test_Pyomo.py
+++ Basic/Test_Pyomo.py
@@ -12,13 +12,13 @@ model = pyo.ConcreteModel()
 
 """ Create Variables """
 # Variables
-model.x = pyo.Var(within=pyo.NonNegativeReals,initialize=9.01) # 10<x<inf
+model.x = pyo.Var(within=pyo.NonNegativeReals,initialize=10.01) # 10<x<inf
 model.y = pyo.Var(within=pyo.NonNegativeReals,initialize=1.57*2) # 3/2pi
 
 """ Create Constraints """
 #constraints
 def xregion(model):
-    return model.x>=10.01
+    return model.x>=10 # return model.x==10
 model.Boundx = pyo.Constraint(rule=xregion)
 
 def yregion1(model):
@@ -33,7 +33,7 @@ model.Boundy2 = pyo.Constraint(rule=yregion2)
 # Objective function
 def obj_rule(model):                                        
     return  model.x + pyo.sin(model.y) # x + sin(y)
-model.obj = pyo.Objective(rule=obj_rule)
+model.obj = pyo.Objective(rule=obj_rule,sense=pyo.minimize) # if you want to maximize objective, use 'sense=pyo.maximize'
 
 """ Solve """
 optimizer = pyo.SolverFactory(solver)
