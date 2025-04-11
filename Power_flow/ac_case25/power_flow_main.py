37587c6acaa1161e5a35128272b814f81bf87476
kwoong2001 <kwoong2001@gmail.com>
2025-04-11 14:38:30 +0900

    250411_v1

---
+++ Power_flow/ac_case25/power_flow_main.py
@@ -0,0 +1,20 @@
+"""
+AC Power flow - 25 bus case V1 (25.04.11)
+
+"""
+
+
+""" Call modules """
+import pyomo.environ as pyo 
+import math
+
+""" INPUTS """
+solver = 'ipopt' # Select solver
+
+
+""" Y 행렬 생성 """
+
+""" 목적함수 및 제약조건 생성"""
+
+"""결과 출력"""
+
