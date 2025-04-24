061d86cd4361979524be2f4f991b77c71074a37e
kwoong2001 <kwoong2001@gmail.com>
2025-04-24 14:08:59 +0900

    250424_V2

--- Optimal_Power_Flow/Distribution/33Bus_bw/OPF_Case_33bw_pyomo.py
+++ Optimal_Power_Flow/Distribution/33Bus_bw/OPF_Case_33bw_pyomo.py
@@ -1,6 +1,6 @@
 """
 OPF_Case_33bw
-V2(예정): PU 단위의 OPF 문제를 정식화하고 결과를 도출하는 코드 구성
+250424_V2: PU 단위의 OPF 문제를 정식화하고 결과를 도출하는 코드 - 33Bus 해 찾기 성공
 
 250422_V1: Pandapower로 부터 불러온 계통 모델을 기반으로 Pyomo에 최적화 문제를 해결할 수 있도록 구성
 """
@@ -27,7 +27,6 @@ Slackbus = 1
 pp.runpp(net,numba=False)
 base_MVA = net._ppc['baseMVA'] #Base MVA
 
-
 # Set values and parameters (Bus, Line, Gen, Load, Ymatrix)
 [Bus_info, Line_info, Gen_info, Load_info, Y_mat_info]=Set_All_Values(np,pd,save_directory,net)
 
@@ -47,6 +46,28 @@ print('Solving OPF model...')
 Problem = optimizer.solve(instance,tee=True)
 
 for bus in Bus_info.index:
-    print(instance.PGen[bus].value)
+    
+    if instance.V_mag[bus].value >= 1e-4:
+        vmag = instance.V_mag[bus].value
+    else:
+        vmag = 0
+    
+    if np.absolute(instance.V_ang[bus].value) >= 1e-4:
+        vang = instance.V_ang[bus].value * 180 / np.pi
+    else:
+        vang = 0
+    
+    print(f"{bus}-Bus Voltage magnitude: {vmag:.4f} [pu], angle: {vang:.2f} [deg]")
+
+
+for bus in Bus_info.index:
+    
+    if instance.PGen[bus].value >= 1e-4:
+        pgen = instance.PGen[bus].value * base_MVA
+    else:
+        pgen = 0
+    print(f"{bus}-Bus Generation: {pgen}MW")
+    
+     
 
 print("solve done!")
--- Optimal_Power_Flow/Distribution/33Bus_bw/Packages/OPF_Creator.py
+++ Optimal_Power_Flow/Distribution/33Bus_bw/Packages/OPF_Creator.py
@@ -1,12 +1,14 @@
 """
 OPF model creator
-V2(예정): PU 단위에서 OPF를 풀 수 있도록 구성
- - 정식화 부분을 다시 살펴보기
+250424_V2: PU 단위에서 OPF를 풀 수 있도록 구성
 
 250422_V1: 모선 전압의 제곱 합 최소화 최적화 문제
    - 최적해는 모선전압의 0.95배에서 결정됨
+   
+제약조건 확인하는 방법: instance.(제약조건 변수 이름).display()
+예시: instance.P_gen_limit_con.display()
 """
-
+  
 def OPF_model_creator(pyo,base_MVA,Slackbus,Bus_info,Line_info,Load_info,Gen_info):
 
     model = pyo.AbstractModel() #dat 파일을 무조건 사용해야 함
@@ -19,37 +21,45 @@ def OPF_model_creator(pyo,base_MVA,Slackbus,Bus_info,Line_info,Load_info,Gen_inf
 
     model.Bus_G = pyo.Param(model.Buses,model.Buses,within=pyo.Any) # Network conductivity matrix
     model.Bus_B = pyo.Param(model.Buses,model.Buses,within=pyo.Any) # Network susceptance matrix
-    model.Lines_i = pyo.Param(model.Lines,within = pyo.Any)     #Lines sending buses
-    model.Lines_j = pyo.Param(model.Lines,within = pyo.Any)     #Lines receiving buses
-
+    
     """
     Voltage
     """
-    # Voltagae initialization rules
-    # Voltage real and imaginary part definition
-    model.V_re = pyo.Var(model.Buses,initialize = 1)  # Voltage magnitude
-    model.V_im = pyo.Var(model.Buses,initialize = 0)  # Voltage angle
-
-    def V_mag_2_rule(model, i):
-        return model.V_re[i]**2 + model.V_im[i]**2
-    model.V_mag_2 = pyo.Expression(model.Buses, rule=V_mag_2_rule)
-        
+    # Voltage variable
+    model.V_mag = pyo.Var(model.Buses,within=pyo.NonNegativeReals,initialize = 1)  # Voltage magnitude
+    model.V_ang = pyo.Var(model.Buses,within=pyo.Reals,initialize = 0)  # Voltage angle
+    
     # Voltage limit
     def V_limits_rule(model, i):
-        #if i ==Slackbus:
-        #    return pyo.Constraint.Skip
-        #else:
-        return (Bus_info['min_vm_pu'][i]**2,model.V_mag_2[i],Bus_info['max_vm_pu'][i]**2)
+        return (Bus_info['min_vm_pu'][i],model.V_mag[i],Bus_info['max_vm_pu'][i])
     model.V_limits_con = pyo.Constraint(model.Buses, rule=V_limits_rule)
+    
+    # Slack constraint
+    def Slack_con_rule(model, i):
+        if i == Slackbus:
+            return model.V_ang[i] == 0
+        else:
+            return pyo.Constraint.Skip
+    model.Slack_con = pyo.Constraint(model.Buses, rule=Slack_con_rule)
     
     """
     Power
     """
-    #Generator
-    model.PGen = pyo.Var(model.Buses, within=pyo.NonNegativeReals, initialize=0.0)
+    #Generation variable
+    model.PGen = pyo.Var(model.Buses, within=pyo.Reals, initialize=0.0)
     model.QGen = pyo.Var(model.Buses, within=pyo.Reals, initialize=0.0)
     
-    #Demand
+    #Active power at each node
+    def P_gen_limit_rule(model,i):
+        return ( (sum(Gen_info.loc[n,'min_p_mw']/base_MVA for n in model.Gens if Gen_info.loc[n,'bus'] == i)) , model.PGen[i], (sum(Gen_info.loc[n,'max_p_mw']/base_MVA for n in model.Gens if Gen_info.loc[n,'bus'] == i)) )
+    model.P_gen_limit_con = pyo.Constraint(model.Buses, rule =P_gen_limit_rule)
+    
+    #Reactive power at each node
+    def Q_gen_limit_rule(model,i):
+        return ( (sum(Gen_info.loc[n,'min_q_mvar']/base_MVA for n in model.Gens if Gen_info.loc[n,'bus'] == i)) , model.QGen[i], (sum(Gen_info.loc[n,'max_q_mvar']/base_MVA for n in model.Gens if Gen_info.loc[n,'bus'] == i)) )
+    model.Q_gen_limit_con = pyo.Constraint(model.Buses, rule =Q_gen_limit_rule)
+    
+    #Demand at each node
     def P_demand_rule(model,i):
         return sum(Load_info.loc[d,'p_mw']/base_MVA for d in model.Loads if Load_info.loc[d,'bus']==i)
     model.PDem = pyo.Expression(model.Buses, rule = P_demand_rule)
@@ -57,22 +67,37 @@ def OPF_model_creator(pyo,base_MVA,Slackbus,Bus_info,Line_info,Load_info,Gen_inf
         return sum(Load_info.loc[d,'q_mvar']/base_MVA for d in model.Loads if Load_info.loc[d,'bus']==i)
     model.QDem = pyo.Expression(model.Buses, rule = Q_demand_rule)
 
+    #Active power flow in each line
+    if 1==0:
+        def P_line_flow_sending_rule(model,l):
+            i = Line_info.loc[l,'from_bus']
+            j = Line_info.loc[l,'to_bus']
+            return model.V_mag[i] * model.V_mag[i] * model.Bus_G[i,i] + model.V_mag[i] * model.V_mag[j] * ( model.Bus_G[i,j] * pyo.cos(model.V_ang[i]-model.V_ang[j]) + model.Bus_B[i,j]* pyo.sin(model.V_ang[i]-model.V_ang[j]) )
+        model.P_line_flow_sending = pyo.Expression(model.Lines,rule = P_line_flow_sending_rule)
     
     """
     Balance (Generation - Demand)
     """
-    # Power injection
-    def Psp_rule(model, i):
-        return model.PGen[i] - model.PDem[i] == 0
-    model.Psp = pyo.Constraint(model.Buses,rule=Psp_rule)
+    # Power injection at each node
+    def P_bal_rule(model, i):
+        return model.PGen[i] - model.PDem[i] == model.V_mag[i] * ( sum (model.V_mag[j] * ( model.Bus_G[i,j] * pyo.cos(model.V_ang[i]-model.V_ang[j]) + model.Bus_B[i,j]* pyo.sin(model.V_ang[i]-model.V_ang[j]) ) for j in model.Buses ) )
+    model.P_bal_con = pyo.Constraint(model.Buses,rule=P_bal_rule)
     
-    def Qsp_rule(model, i):
-        return model.QGen[i] - model.QDem[i] == 0
-    model.Qsp = pyo.Constraint(model.Buses,rule=Qsp_rule)
+    def Q_bal_rule(model, i):
+        return model.QGen[i] - model.QDem[i] == model.V_mag[i] * ( sum (model.V_mag[j] * ( model.Bus_G[i,j] * pyo.sin(model.V_ang[i]-model.V_ang[j]) - model.Bus_B[i,j]* pyo.cos(model.V_ang[i]-model.V_ang[j]) ) for j in model.Buses ) )
+    model.Q_bal_con = pyo.Constraint(model.Buses,rule=Q_bal_rule)
+    
+    # Power injection value (Gen - Demand)
+    def P_inj_rule(model,i):
+        return model.PGen[i] - model.PDem[i]
+    model.P_inj = pyo.Expression(model.Buses,rule = P_inj_rule)
+    def Q_inj_rule(model,i):
+        return model.QGen[i] - model.QDem[i]
+    model.Q_inj = pyo.Expression(model.Buses,rule = Q_inj_rule)
     
     
     def Objective_rule(model):
-        return  sum(model.V_mag_2[i] for i in model.Buses)
+        return  sum(model.PGen[i] for i in model.Buses)
         #return model.P_losses_total
         #return  sum(model.P_gen_cost[i] for i in model.Buses)
     model.obj = pyo.Objective(rule=Objective_rule,sense=pyo.minimize)
